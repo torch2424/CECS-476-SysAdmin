6ef06141ba55ee7713690b2bd905055c60177313
student <csa476x1@cheetah.cecs.csulb.edu>
2017-04-04 18:52:08 -0700

    Added the latest homework, and subnet

---
+++ subnet.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+
+start = int(argv[1]) & int(argv[2])
+end = start + (int(argv[2])^255)
+
+print "start is", start
+print "end is", end
+from sys import argv
+
+
+start = int(argv[1]) & int(argv[2])
+end = start + (int(argv[2])^255)
+
+print "start is", start
+print "end is", end
